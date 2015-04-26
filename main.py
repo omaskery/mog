4fc32a9ecb95e96e36591fc3fb872e7832dda478
Oliver Maskery <omaskery@googlemail.com>
2015-04-26 23:41:31 +0100

    forgot this from last commit, d'oh!

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ def main():
 
 
 def mog_init(args):
+    """handles the `mog init` command"""
+
     project_path = os.path.join(args.path, args.name)
 
     if mog.project.Project.already_exists(project_path):
@@ -23,24 +25,44 @@ def mog_init(args):
 
     project = mog.project.Project.create_new(project_path, args.name, args.gm_proj)
 
+    # nothing else needs doing atm?
+    _ = project
+
 
 def mog_scan(args):
+    """handles the `mog scan` command"""
+
     if not mog.project.Project.already_exists(args.path):
         print("no mog project found in specified directory")
         return
 
     project = mog.project.Project(args.path)
 
+    # everything below here is testing and unfinished...
+
     print("scanning '{}'".format(project.project_file.gamemaker_project_path))
+    gm_project = mog.gamemaker.project.Project(project.project_file.gamemaker_project_path)
+    print("objects:")
+    for object in gm_project.objects:
+        print("  Object({})".format(object.name))
+        for event in object.events:
+            print("    Event({}, {}):".format(event.type_name, event.number))
+            for action in event.code_actions:
+                print("      Code Action ({}):\n{}".format(
+                    action['whoName'], action['code']
+                ))
 
 
 def parse_args():
+    """configures the argument parser and parses the command line arguments"""
+
     parser = argparse.ArgumentParser(
         description="""mog (My Own Game-Maker-Language)
         an experiment into producing GML from a made up language"""
     )
     subparsers = parser.add_subparsers(dest="mode", help="the mode of operation")
 
+    # mog init
     parser_init = subparsers.add_parser(
         'init', help='initialises a new mog project'
     )
@@ -57,6 +79,7 @@ def parse_args():
     )
     parser_init.set_defaults(func=mog_init)
 
+    # mog scan
     parser_scan = subparsers.add_parser(
         'scan', help='scans the game maker project for new objects'
     )
@@ -66,6 +89,7 @@ def parse_args():
     )
     parser_scan.set_defaults(func=mog_scan)
 
+    # go go go
     return parser.parse_args()
 
 
